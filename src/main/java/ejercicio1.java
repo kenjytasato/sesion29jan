fb0c82fc2eef67c8e8ca50fcae0cebd075a231dd
Kenjy Tasato Canepa <kenjytasatocanepa@Kenjys-MacBook-Pro-2.local>
2021-01-15 18:50:01 -0500

    adding excercise

--- src/main/java/ejercicio1.java
+++ src/main/java/ejercicio1.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class ejercicio1 {
+public class ejercicio1 {
+
+    //ejercicio 1
+
+    //colocar el implicit wait como un metodo
+    //buscar macbook
+    //comparar los resultados de la primera pagina
+    //entrar al resultado mas barato
+    //imprimir por consola el titulo del producto y cuantos colores
+    // tiene disponible
+
+//ejercicio 2
+
+    //entrar a https://www.saucedemo.com/
+    //agregar los 6 items al carrito de compras
+    //listar por consola los items de forma ascendente y descendete por nombres
+    //ir al carrito de compras
+    //remover los primeros 3 items
+    //ir a checkout
+    //llenar los datos del formulario
+    //verificar que el total es la suma de los subtotales
+    //terminar la compra y verificar que muestre el mensaje
+    //THANK YOU FOR YOUR ORDER
+
 }
